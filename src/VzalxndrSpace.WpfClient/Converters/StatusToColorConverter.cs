6e4d20608fdd3bce7356cf8d225719beb67b34d2
vzalxndr <vzalxndr.reserve@gmail.com>
2026-03-15 18:24:40 +0100

    feat(client): add goal status converters and logout button

---
+++ src/VzalxndrSpace.WpfClient/Converters/StatusToColorConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace VzalxndrSpace.WpfClient.Converters
+{
+    public class StatusToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int status)
+            {
+                return status switch
+                {
+                    0 => Brushes.DarkOrange, // in progress
+                    1 => Brushes.MediumSeaGreen, // completed
+                    2 => Brushes.DarkRed, // cancelled
+                    _ => Brushes.Gray, // undefined
+                };
+            }
+            return Brushes.Black;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        { 
+            throw new NotImplementedException();
+        }
+    }
+}
